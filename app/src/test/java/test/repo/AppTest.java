cd74a55e5e71cc5f865f028e77dbd68735c24863
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2023-04-12 12:44:37 +0200

    adds new test

--- app/src/test/java/test/repo/AppTest.java
+++ app/src/test/java/test/repo/AppTest.java
@@ -11,4 +11,9 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void appHasAGreeting2() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
 }
